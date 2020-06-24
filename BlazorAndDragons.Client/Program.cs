9780a94f3933ad89e7541c60a682486812b60ccf
Davide Guida <mizrael@yahoo.it>
2020-06-23 22:45:28 -0400

    added configuration, minor refactorings

--- BlazorAndDragons.Client/Program.cs
+++ BlazorAndDragons.Client/Program.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BlazorAndDragons.Client
@@ -11,9 +14,22 @@ namespace BlazorAndDragons.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+           
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(sp =>
+            {
+                var config = sp.GetRequiredService<IConfiguration>();
+                var serverUrl = config["ServerUrl"];
+
+                var channel = GrpcChannel.ForAddress(serverUrl, new GrpcChannelOptions
+                {
+                    HttpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler())
+                });
+
+                var client = new Classes.ClassesClient(channel);
+                return client;
+            });
 
             await builder.Build().RunAsync();
         }
